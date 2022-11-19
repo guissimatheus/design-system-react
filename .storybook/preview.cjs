c7c1571b28d501cc1016e4068a0f8558e7b20c38
Matheus Guissi <guissimatheus@gmail.com>
2022-11-19 20:40:22 -0300

    Tentativa de correção de mock

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -3,10 +3,20 @@ import { initialize, mswDecorator } from 'msw-storybook-addon';
 
 import '../src/styles/global.css'
 
+let options = {};
+if (location.hostname === "guissimatheus.github.io") {
+  options = {
+    serviceWorker: {
+      url: "/design-system-react/mockServiceWorker.js"
+    }
+  }
+}
+
 // Initialize MSW
 initialize({
   // Remove log das requisições não tratadas
-  onUnhandledRequest: 'bypass'
+  onUnhandledRequest: 'bypass',
+  ...options
 });
 
 // Provide the MSW addon decorator globally
